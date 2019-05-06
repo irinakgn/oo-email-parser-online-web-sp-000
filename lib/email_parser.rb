fe461b880d020413704972e45672052fc1312852
Irina Kagan <irinagamb@gmail.com>
2019-05-06 00:32:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,15 +5,6 @@
 
 
 class EmailParser
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
+  attr_accessor :name
 
 end
